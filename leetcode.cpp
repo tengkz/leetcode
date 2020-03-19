e056e4dd94bc0cd8871be22bd3db7fc2638e49da
tengkz <tengkz@gmail.com>
2020-03-19 22:44:51 +0800

    add problem 7

--- leetcode.cpp
+++ leetcode.cpp
@@ -119,6 +119,19 @@ public:
         }
         return result;
     }
+
+    int reverse(int x) {
+        int y=0;
+        while(x){
+            if(y>214748364 || y<-214748364){
+                return 0;
+            }
+            y=y*10+x%10;
+            x/=10;
+        }
+        return y;
+    }
+
 };
 
 int main()
